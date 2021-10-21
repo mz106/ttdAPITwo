79351269b279b6a1f08d954c4be42f8d9e15ed26
mz106 <flashman123@fastmail.fm>
2021-10-21 19:35:12 +0100

    bug fix

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
-const { application } = require("express");
-const express = require("express");
+const express = require('express')
 
-const app = express();
+const app = express()
 
 app.listen(3000, () => {
     console.log('app is listening')
