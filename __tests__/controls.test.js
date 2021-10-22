9f32d921ba2d18fc6945d034a3c9fe0e80415a6a
mz106 <flashman123@fastmail.fm>
2021-10-22 11:25:39 +0100

    home test added

---
+++ __tests__/controls.test.js
@@ -0,0 +1,14 @@
+
+const { home, hello } = require('../controllers/controls.js')
+
+describe('Test controls.js', () => {
+  test('responds to /', () => {
+    const req = { }
+    const res = {
+      text: '',
+      send: function (input) { this.text = input }
+    }
+    home(req, res)
+    expect(res.text).toEqual('Hello, world!')
+  })
+})
