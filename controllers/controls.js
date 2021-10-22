ba2b912765f8b3076d2bf2b1ceedd8a10477561f
mz106 <flashman123@fastmail.fm>
2021-10-22 10:51:00 +0100

    routes added standard run

--- controllers/controls.js
+++ controllers/controls.js
@@ -1,14 +1,14 @@
 
 const home = (req, res) => {
-    res.send('Hello, world!')
+  res.send('Hello, world!')
 }
 
 const hello = (req, res) => {
-    const name = req.params.name || 'world'
-    res.send(`Hello ${name}`)
+  const name = req.params.name || 'world'
+  res.send(`Hello ${name}`)
 }
 
 module.exports = {
-    home,
-    hello
+  home,
+  hello
 }
