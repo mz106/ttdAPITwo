38bea552bc33bb001fb4b561a0fe27136b3845f8
mz106 <flashman123@fastmail.fm>
2021-10-22 10:47:38 +0100

    routes added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 const express = require('express')
+const router = require('./routes/routes.js')
 
 const app = express()
+
+app.use('/', router)
 
 app.listen(3000, () => {
   console.log('app is listening')
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,10 @@
 
 const express = require('express')
+const { home, hello } = require('../controllers/controls.js')
 
 const router = express.Router()
+
+router.get('/', home)
+router.get('/hello/:name', hello)
 
 module.exports = router
